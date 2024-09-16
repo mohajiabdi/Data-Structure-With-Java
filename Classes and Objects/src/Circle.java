beafd4fec0f5c2ae064681940f559d5462b3847c
mohajiabdi <135143797+mohajiabdi@users.noreply.github.com>
2024-09-16 19:11:00 +0300

    new

---
+++ Classes and Objects/src/Circle.java
@@ -0,0 +1,27 @@
+public class Circle {
+    // Declare private instance variables
+    private double radius;
+
+    // Declare public constructor
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public Circle(){
+        this.radius = 1.0;
+    }
+
+    // Declare public method to calculate and return the area
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    // Declare public method to calculate and return the Parameters
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    // Declare public method to set the radius
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
---
+++ Classes and Objects/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Create a Circle object with radius 5
+        Circle circle = new Circle();
+
+        // Print the area of the circle
+        circle.setRadius(3);
+        System.out.printf("\nThe area of the circle is %.4f" , circle.getArea());
+        System.out.printf("\nThe Perimeter of the circle is %.4f" , circle.getPerimeter());
+    }
+}
