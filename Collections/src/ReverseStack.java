01ee8539aaed75bb516616ee2f2fd09c800564b8
mohajiabdi <135143797+mohajiabdi@users.noreply.github.com>
2024-10-18 20:57:12 +0300

    ok

--- Collections/src/ReverseStack.java
+++ Collections/src/ReverseStack.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class ReverseStack {
     public static void main(String[] args) {
         System.out.println(reverse("somalia"));
-        
+
     }
     static String reverse (String input){
         Stack <Character> s1 = new Stack<Character>();
