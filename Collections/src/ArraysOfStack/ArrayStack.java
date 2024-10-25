53261840be0b2a3627e09103727585ea57a65784
mohajiabdi <135143797+mohajiabdi@users.noreply.github.com>
2024-10-25 19:53:50 +0300

    ok

--- Collections/src/ArraysOfStack/ArrayStack.java
+++ Collections/src/ArraysOfStack/ArrayStack.java
@@ -1,13 +1,17 @@
 package ArraysOfStack;
 
+import java.util.Arrays;
+
 public class ArrayStack <T> {
     private final static int DEFAUL_CAPACITY =2;
     private int top;
-    T[] stack;
+    T[] stack,merged;
 
     public ArrayStack(int initial_Capacity){
         this.top = 0;
         stack = (T[])(new Object[initial_Capacity]);
+        merged = (T[])(new Object[initial_Capacity]);
+
     }
 
     public ArrayStack(){
@@ -52,12 +56,12 @@ public class ArrayStack <T> {
         return stack[this.top - 1];
     }
     //display stack
-    void display(){
-        for(int i=this.top-1; i >= 0; i--){
-            System.out.print(stack[i] + " ");
-        }
-        System.out.println();
-    }
+//    void display(){
+//        for(int i=this.top-1; i >= 0; i--){
+//            System.out.print(stack[i] + " ");
+//        }
+//        System.out.println();
+//    }
 
     private void resize() {
         // Double the capacity of the current stack
@@ -71,5 +75,61 @@ public class ArrayStack <T> {
 
         // Replace the old array with the new array
         stack = newStack;
+    }
+
+    // Assignment
+
+    //IsFull Method
+    boolean isFull(){
+        return top == stack.length;
+    }
+
+    //GetCapacity
+    int getCapacity(){
+        return stack.length;
+    }
+
+    //Display from Top to down
+    void display(){
+        for(int i=this.top-1; i >= 0; i--){
+            System.out.print(stack[i] + " \t");
+        }
+        System.out.println();
+    }
+
+    void find(String element){
+        int count =0;
+        for(int i = 0; i< top; i++){
+            if(stack[i] == element)
+              count++;
+        }
+
+//        if(count ==0){
+//                System.out.println("\nNot Found!!");
+//        }else{
+//        System.out.println();
+//        System.out.println(element + " is Found \'"+ count + "\' Times" );
+//    }
+        System.out.println();
+        System.out.println(count == 0? "Not Found!!!":
+                element+" is Found \'"+ count+"\' Times");
+}
+
+T[] merge(T[] stack, T[] stack2){
+        //int[] mergedLength = new int[stack.length + stack2.length];
+       // System.arraycopy(stack,0,mergedLength,stack.length,stack2.length);
+merged =(T[]) new Object[stack.length + stack2.length];
+//    System.arraycopy(stack,0,this.stack,0,this.stack.length);
+//    System.arraycopy(stack2,0,this.stack,stack.length,this.stack.length);
+    System.arraycopy(stack,0,merged,0,stack.length);
+    System.arraycopy(stack2,0,merged,stack.length,stack.length);
+    return merged;
+    }
+
+    void  fullName(T[] stack, T[]stack2){
+       T[] merged =(T[]) new Object[stack.length + stack2.length];
+        for(int i=0; i <top; i++){
+            System.out.println(stack[i] +" "+stack2[i]);
+        }
     }
 }
--- Collections/src/ArraysOfStack/Main.java
+++ Collections/src/ArraysOfStack/Main.java
@@ -3,22 +3,36 @@ package ArraysOfStack;
 public class Main {
     public static void main(String[] args) {
         ArrayStack myStack = new ArrayStack(2);
+        ArrayStack myStack2 = new ArrayStack(2);
 
         System.out.println(myStack.isEmpty());
 
         myStack.push("Ali");
         myStack.push("Mo");
         myStack.push("Haji");
+        myStack.push("Haji");
+        myStack.push("Haji");
         myStack.push("Abdi");
         myStack.push("Amin");
         myStack.push("Asho");
+        myStack.push("Asho");
+
+        myStack2.push(19);
+        myStack2.push(18);
+        myStack2.push(100);
+        myStack2.push(25);
 
 
       //  System.out.println(myStack.isEmpty());
 
-        System.out.println(myStack.pop());
-        System.out.println(myStack.Size());
+      //  System.out.println(myStack.pop());
+        //System.out.println(myStack.Size());
+       // System.out.println(myStack.isFull());
+       // System.out.println(myStack.getCapacity());
 
+        //merge(myStack,myStack2);
+       // myStack.fullName(myStack,myStack2);
         myStack.display();
+        //myStack.find("Asho");
     }
 }
